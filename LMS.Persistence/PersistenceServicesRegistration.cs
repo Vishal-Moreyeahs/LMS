f661fb5b1ba4006149826d1c5aaf61eb7ca6cf73
Vishal-Moreyeahs <vishal.pawar@moreyeahs.com>
2023-12-21 12:46:20 +0530

    Initial Setup

---
+++ LMS.Persistence/PersistenceServicesRegistration.cs
@@ -0,0 +1,23 @@
+﻿using LMS.Application.Contracts.Persistence;
+using LMS.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LMS.Persistence
+{
+    public static class PersistenceServicesRegistration
+    {
+        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DataBaseContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DBConnection"))
+            );
+
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+    }
+}
---
+++ LMS.Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LMS.Application.Contracts.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace LMS.Persistence.Repositories
+{
+    
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DataBaseContext _dbContext;
+
+        public UnitOfWork(DataBaseContext context)
+        {
+            _dbContext = context;
+        }
+
+        public async Task<int> Save()
+        {
+            return await _dbContext.SaveChangesAsync();
+        }
+    }
+}
