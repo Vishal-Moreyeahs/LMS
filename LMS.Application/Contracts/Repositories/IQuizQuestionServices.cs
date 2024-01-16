8386f67e4b7821ce8eee9a0f2797e5d09b3b4cc0
Vishal-Moreyeahs <vishal.pawar@moreyeahs.com>
2024-01-17 00:15:50 +0530

    Add QuizQuestion api's

--- LMS.Application/Contracts/Repositories/IQuizQuestionServices.cs
+++ LMS.Application/Contracts/Repositories/IQuizQuestionServices.cs
@@ -4,13 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using LMS.Application.Models;
+using LMS.Application.Request;
 
 namespace LMS.Application.Contracts.Repositories
 {
     public interface IQuizQuestionServices
     {
         Task<Response<dynamic>> GetQuizQuestions(int quizId);
-        Task<Response<dynamic>> AssignQuestionToQuiz();
-        Task<Response<dynamic>> DeleteQuestionFromQuiz();
+        Task<Response<dynamic>> AssignQuestionToQuiz(List<QuizQuestionRequest> quizQuestionRequests); //https://community.canvaslms.com/t5/Instructor-Guide/How-do-I-create-a-quiz-with-individual-questions/ta-p/1248
+        Task<Response<dynamic>> DeleteQuestionFromQuiz(int quizQuestionId);
     }
 }
--- LMS.Application/Services/QuizQuestionManager/QuizQuestionServices.cs
+++ LMS.Application/Services/QuizQuestionManager/QuizQuestionServices.cs
@@ -7,6 +7,8 @@ using AutoMapper;
 using LMS.Application.Contracts.Persistence;
 using LMS.Application.Contracts.Repositories;
 using LMS.Application.Models;
+using LMS.Application.Request;
+using LMS.Domain.Models;
 
 namespace LMS.Application.Services.QuizQuestionManager
 {
@@ -22,19 +24,67 @@ namespace LMS.Application.Services.QuizQuestionManager
             _mapper = mapper;
             _unitOfWork = unitOfWork;
         }
-        public Task<Response<dynamic>> AssignQuestionToQuiz()
+
+        public async Task<Response<dynamic>> AssignQuestionToQuiz(List<QuizQuestionRequest> quizQuestionRequests)
         {
-            throw new NotImplementedException();
+            if (quizQuestionRequests.Count == 0 || quizQuestionRequests == null)
+            { 
+                throw new ApplicationException("Invalid Request Format");
+            }
+
+            //await _unitOfWork.GetRepository<QuizQuestion>()
+
+            return null;
         }
 
-        public Task<Response<dynamic>> DeleteQuestionFromQuiz()
+        public async Task<Response<dynamic>> DeleteQuestionFromQuiz(int quizQuestionId)
         {
-            throw new NotImplementedException();
+            var quizQuestion = await _unitOfWork.GetRepository<QuizQuestion>().Get(quizQuestionId);
+            if (quizQuestion == null || !quizQuestion.IsActive)
+            {
+                throw new ApplicationException("Quiz question not found");
+            }
+
+            quizQuestion.IsActive = false;
+            await _unitOfWork.GetRepository<QuizQuestion>().Update(quizQuestion);
+            var isQuizQuestionDeleted = await _unitOfWork.SaveChangesAsync();
+
+            if (isQuizQuestionDeleted <= 0)
+            {
+                throw new ApplicationException("Quiz Question not deleted");
+            }
+
+            var response = new Response<dynamic>
+            {
+                Status = true,
+                Message = "Quiz question deleted successfully", 
+            };
+            return response;
         }
 
-        public Task<Response<dynamic>> GetQuizQuestions(int quizId)
+        public async Task<Response<dynamic>> GetQuizQuestions(int quizId)
         {
-            var 
+            var loggedInUser = await _authenticatedUserService.GetLoggedInUser();
+
+            var quiz = await _unitOfWork.GetRepository<Quiz>().Get(quizId);
+
+            if (quiz == null || !quiz.IsActive || quiz.Company_Id != loggedInUser.CompanyId)
+            {
+                throw new ApplicationException("Quiz not found");
+            }
+
+            var quizQuestion = _unitOfWork.GetRepository<QuizQuestion>().GetAllRelatedEntity();
+
+            quizQuestion = quizQuestion.Where(x => x.Quiz_Id == quizId && x.IsActive).OrderBy(x => x.SequenceNo).ToList();
+
+            var questions = quizQuestion.Select(x => x.QuestionBank).ToList();
+
+            var response = new Response<dynamic> { 
+                Status = true,
+                Message = "Question for Quiz is retreived successfully",
+                Data = questions
+            };
+            return response;
         }
     }
 }
